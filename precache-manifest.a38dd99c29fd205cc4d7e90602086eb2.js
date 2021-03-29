b51ca09eeef3e932ff3e40a366cf9f8af1eccdc2
sm67nono <manna.subhadeep@gmail.com>
2021-03-29 12:08:26 +0200

    Updates

---
+++ precache-manifest.a38dd99c29fd205cc4d7e90602086eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145a4cdf14737c440a731ccee442b990",
+    "url": "/datamart-ui-react/index.html"
+  },
+  {
+    "revision": "845954ce2f33fb6adffa",
+    "url": "/datamart-ui-react/static/css/main.c0632363.chunk.css"
+  },
+  {
+    "revision": "22e794abac8dc08ae039",
+    "url": "/datamart-ui-react/static/js/2.4e1f3783.chunk.js"
+  },
+  {
+    "revision": "1b47cb38a47627b59fa3de3eb3b9bc15",
+    "url": "/datamart-ui-react/static/js/2.4e1f3783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845954ce2f33fb6adffa",
+    "url": "/datamart-ui-react/static/js/main.96f8780b.chunk.js"
+  },
+  {
+    "revision": "c29b8e319e7983a061f4",
+    "url": "/datamart-ui-react/static/js/runtime-main.73c45287.js"
+  }
+]);
